0cc5aa1309ca26a985d64935bce54561efc64572
James DeLorenzo <jdelo417@gmail.com>
2023-06-21 19:44:23 -0400

    add heartbeat and helloworld

---
+++ hello_world/helloworld.vhdl
@@ -0,0 +1,17 @@
+-- hello world program
+use std.textio.all;
+
+-- define design entity with no ports
+entity hello_world is
+end hello_world;
+
+architecture behaviour of hello_world is
+begin
+    process
+        variable l : line;
+    begin
+        write (l, String'("Hello world!"));
+        writeline (output, l);
+        wait;
+    end process;
+end behaviour;
